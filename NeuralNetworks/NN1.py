bea1decb97f167762acd328688c02d0549f1ce61
Siddartha Reddy <reddy.siddartha53@gmail.com>
2020-06-23 20:04:54 -0700

    scaled down data(0,1) to work well with NN

--- NeuralNetworks/NN1.py
+++ NeuralNetworks/NN1.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-#keras is an hihglevel API for tensorflow, to write less code, no def tensors
+#keras is an higglevel API for tensorflow, to write less code, no def tensors
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
@@ -7,8 +6,11 @@ data = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = data.load_data()
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#scale down data to scale of (0,1) to work well with the NN
 train_images = train_images/255.0
 test_images = test_images/255.0
+
 print(len(train_images))
 # for i in range(10):
 #     print(train_images[i])
